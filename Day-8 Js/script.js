b02a4d7217537fc99b103deaf5ce7dea50a6177f
Waleed Sheikh <125444427+waleedsheikh30@users.noreply.github.com>
2025-08-19 17:54:21 +0500

    Add files via upload

---
+++ Day-8 Js/script.js
@@ -0,0 +1,41 @@
+// Output ways
+// alert("Hello world");
+console.log("My name is waleed");
+document.write("This is me");
+document.getElementById("para").innerHTML = "HEllO This is me";
+document.getElementsByClassName("para").innerHTML = "HEllO This is me";
+
+
+// variables -> to store data
+// 3 keywords -> var, let and const
+const pi = 3.14;
+
+let c = 34;
+let d = 49;
+
+console.log(c+d);
+
+
+console.log(typeof(pi));
+console.log(pi);
+
+
+
+
+// data types in Js
+// String -> "This is string"
+let myname = "Waleed";
+// Number -> 20, 30, 12345
+let a = 20;
+// Boolean -> True, False
+let isActive = false;
+// Undefined -> variable without value
+let notdefined;
+// Null -> empty value
+let empty = null;
+// Object -> {key: value}
+let object = {myname: "Waleed", age: 20};
+// Array -> [1,2,3]
+let arr = ["blue", "orange", "green"];
+
+console.log(myname, a, isActive, notdefined, empty, object, arr);
