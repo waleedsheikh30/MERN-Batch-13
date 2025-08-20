5a9de054c57ca51923c5f41df322f7792f8be194
Waleed Sheikh <125444427+waleedsheikh30@users.noreply.github.com>
2025-08-21 02:51:38 +0500

    Add files via upload

---
+++ Day-9 Js/script.js
@@ -0,0 +1,36 @@
+let a = 5;
+let b = "5";
+
+// comaprison operator (==, ===, !=) output -> true or false
+
+// chk only value stored in variable
+console.log(a == b); 
+
+// chk value and data type of variable
+console.log(a === b);
+
+console.log(a != b);
+
+
+// += , -=, *=
+
+// a -> a = a + b
+
+// agr ik variable ki value num hai or dosry variable ki value string hai to agr in dono variaables k drmiyan hm + use krain gy to output me wo dono values ko concatinate kr de gha 
+console.log(a += b);
+console.log(a -= b);
+console.log(a *= b);
+
+let isStudent = true;
+let isPresent = false;
+
+// logical operator &&, ||, !
+console.log(isStudent && isPresent);
+
+console.log(isStudent || isPresent);
+
+console.log(!isStudent);
+
+console.log(a > b);
+
+console.log(a < b);
