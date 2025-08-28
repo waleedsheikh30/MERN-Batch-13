1bd14a50fb8e43b56d414dadd6b4fbc5c801cbd0
Waleed Sheikh <125444427+waleedsheikh30@users.noreply.github.com>
2025-08-28 17:42:52 +0500

    Add files via upload

---
+++ Day-11 Js/script.js
@@ -0,0 +1,31 @@
+// for loop -> jb apko condition ka pta k kahn se le kr khn tk chlni hai
+// for (let i = 0; i <= 5; i++) {
+//     console.log("Number: " + i);
+// }
+
+// break the loop
+// for (let i = 0; i <= 5; i++) {
+//     if(i === 3) break;
+//     console.log(i);
+// }
+
+// skip that value and prints all values instead of this
+// for (let i = 0; i <= 5; i++) {
+//     if(i === 3) continue;
+//     console.log(i);
+// }
+
+// while loop -> jb tk condition flase ni ho jati tb tk loop chly gi
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// do while loop -> similar to while but runs at least once
+// let i = 1;
+// do {
+//     console.log("Do while count: " + i);
+//     i++;
+// } while (i <= 5);
+
